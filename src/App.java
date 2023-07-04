638cdc6ad89e5cf49141cc33634abb9439b9121b
Facuzanan88 <facundozanandrea@gmail.com>
2023-07-04 00:02:09 -0300

    Logica conversor de Monedas

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 import javax.swing.JOptionPane;
 
 public class App {
@@ -10,6 +12,27 @@ public class App {
         if (elige == 2) {
             JOptionPane.showMessageDialog(null, "Programa cerrado");
             System.exit(0);
+        }
+
+        if (elige == 0) {
+            DecimalFormat formateoDivisa = new DecimalFormat("#.##");
+            boolean continuarPrograma = true;
+
+            while (continuarPrograma) {
+                String[] opciones = { "Pesos a Dolares", "Pesos a Euros", "Pesos a Yeanes", "Pesos a Reales" };
+                String cambio = (String) JOptionPane.showInputDialog(null, "Elija una Opcion", "Conversor de Monedas",
+                        JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+
+                String input = JOptionPane.showInputDialog(null, "Ingrese la cantidad de Pesos");
+                double pesos;
+
+                try {
+                    pesos = Double.parseDouble(input);
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Valor no valido");
+                }
+
+            }
         }
     }
 }
